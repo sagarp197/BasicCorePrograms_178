9f93e7d0775dc07363db173e8aa82db1166d9213
sagarp197 <sagarp197@gmail.com>
2022-08-08 06:03:03 +0530

    Program file added for Basic Core Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FlipCoin.DisplayPercentage();
+            //LeapYear.DisplayYearIsLeap();
+            //PowerOfTwo.PrintTableOfPower2();
+            //HarmonicNumber.DisplayNthHarmonicNumber();
+            Factors.DisplayPrimeFactor();
+        }
+    }
+}
